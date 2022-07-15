863add4d51e136b2e7d0dc328b6dee4283a7b7ca
YoRHaUnitA0 <maximpshek@gmail.com>
2022-07-16 00:37:34 +0300

    initCommit

---
+++ Assets/Scripts/GlobalSelectablesController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class GlobalSelectablesController
+{
+    private static GameObject _selectedUnit = null;
+
+    // Returns PREVIOUSLY SELECTED OBJECT
+    public static GameObject Select(GameObject o)
+    {
+        var prev = _selectedUnit;
+        _selectedUnit = o;
+        return prev;
+    }
+
+    public static GameObject GetSelected() => _selectedUnit;
+    public static void ResetSelected() => _selectedUnit = null;
+}
---
+++ Assets/Scripts/SelectionScript.cs
@@ -0,0 +1,116 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class SelectionScript : MonoBehaviour
+{
+    private enum HoverState
+    {
+        None,
+        Highlighted,
+        Selected
+    }
+
+    public Sprite defaultSprite;
+    public Sprite selectedSprite;
+    public Sprite hoverSprite;
+
+    [ShowInInspector, ReadOnly] private HoverState _hoverState = HoverState.None;
+
+    private SpriteRenderer _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void EnableSelectedOutline()
+    {
+        _renderer.sprite = selectedSprite;
+    }
+
+    private void DisableSelectedOutline()
+    {
+        _renderer.sprite = defaultSprite;
+    }
+
+    private void DisableHoverOutline()
+    {
+        _renderer.sprite = defaultSprite;
+    }
+
+    private void EnableHoverOutline()
+    {
+        _renderer.sprite = hoverSprite;
+    }
+
+    // These are USED, just called by RaycastController
+    public void _OnMouseOver()
+    {
+    }
+
+    public void _OnMouseExit()
+    {
+        if (_hoverState == HoverState.Highlighted)
+        {
+            DisableHoverOutline();
+            _hoverState = HoverState.None;
+        }
+    }
+
+    public void _OnMouseEnter()
+    {
+        if (_hoverState == HoverState.None)
+        {
+            EnableHoverOutline();
+            _hoverState = HoverState.Highlighted;
+        }
+    }
+
+    public void DeselectUnit(bool reset = false)
+    {
+        DisableSelectedOutline();
+        _hoverState = HoverState.None;
+    }
+
+    private void HandlePrev(GameObject prev)
+    {
+        EnableSelectedOutline();
+        _hoverState = HoverState.Selected;
+
+        if (prev == null) return;
+        prev.GetComponent<SelectionController>()?.DisableSelectedOutline();
+        prev.GetComponent<SelectionController>()?.DeselectUnit();
+    }
+
+    private void Select()
+    {
+        DisableHoverOutline();
+
+        var previous = GlobalSelectablesController.Select(gameObject);
+        HandlePrev(previous);
+    }
+
+    public void _OnMouseClick()
+    {
+        switch (_hoverState)
+        {
+            case HoverState.Highlighted:
+                Select();
+                break;
+            case HoverState.Selected:
+                DeselectUnit();
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            _OnMouseClick();
+
+        if (Input.GetKeyDown(KeyCode.Escape) && Selected)
+            DeselectUnit(true);
+    }
+
+    private bool Selected => GlobalSelectablesController.GetSelected() == gameObject;
+}
