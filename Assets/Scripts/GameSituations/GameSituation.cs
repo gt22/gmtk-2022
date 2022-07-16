a7642f4e9f98463c0dc015ec5f0f010095d6f5f5
YoRHaUnitA0 <maximpshek@gmail.com>
2022-07-16 04:49:57 +0300

    game situation basic implementation

---
+++ Assets/Scripts/GameSituations/GameSituation.cs
@@ -0,0 +1,31 @@
+using Global;
+using UnityEngine;
+
+public class GameSituation : MonoBehaviour
+{
+    public int turnsUntilTrigger;
+    private int _turnsToTrigger;
+
+    private void OnEnable()
+    {
+        _turnsToTrigger = turnsUntilTrigger;
+        TurnHandler.OnAfterTurnEnd += ReduceTurns;
+    }
+
+    private void OnDisable()
+    {
+        TurnHandler.OnAfterTurnEnd -= ReduceTurns;
+    }
+
+    private void ReduceTurns()
+    {
+        _turnsToTrigger--;
+        if (_turnsToTrigger == 0)
+            SituationEffect();
+    }
+
+    private void SituationEffect()
+    {
+        
+    }
+}
